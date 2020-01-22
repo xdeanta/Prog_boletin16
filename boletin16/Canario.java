441cdb613eaa1870c554df7027f08ce26eb990b0
Xavier De Anta <xdeantabelisario@danielcastelao.org>
2020-01-22 10:23:02 +0100

    Primer Codigo

---
+++ boletin16/Canario.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin16;
+
+/**
+ *
+ * @author xdeantabelisario
+ */
+public class Canario implements IpuedeCantar{
+    @Override
+    public void cantar(){
+        System.out.println("Chirp");
+    }
+}
---
+++ boletin16/Persona.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin16;
+
+/**
+ *
+ * @author xdeantabelisario
+ */
+public class Persona implements IpuedeCantar{
+    @Override
+    public void cantar(){
+        System.out.println("Do Re Mi Fa Sol La Si");
+    }
+    
+}
